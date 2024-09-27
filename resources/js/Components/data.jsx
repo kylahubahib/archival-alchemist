d853e2b09b974661485f09fa59903d897b37b6bc
Kyla Hubahib <kyla.hubahib18@gmail.com>
2024-09-28 02:38:19 +0800

    changes

---
+++ resources/js/Components/data.jsx
@@ -0,0 +1,17 @@
+export const dateFilters = [
+    {key: "All", label: "All"},
+    {key: "Today", label: "Today"},
+    {key: "This Week", label: "This Week"},
+    {key: "This Month", label: "This Month"}
+  ];
+
+  export const ratingFilters = [
+    {key: "All", label: "All"},
+    {key: "Highest", label: "Highest"},
+    {key: "Lowest", label: "Lowest"},
+    {key: 1, label: "1"},
+    {key: 2, label: "2"},
+    {key: 3, label: "3"},
+    {key: 4, label: "4"},
+    {key: 5, label: "5"}
+  ];
