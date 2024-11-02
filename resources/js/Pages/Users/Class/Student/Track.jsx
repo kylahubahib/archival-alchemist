c606f67bd3a567884434eb833899d8821d7c0515
Kyla Hubahib <kyla.hubahib18@gmail.com>
2024-11-02 22:09:00 +0800

    ok

--- resources/js/Pages/Users/Class/Student/Track.jsx
+++ resources/js/Pages/Users/Class/Student/Track.jsx
@@ -155,13 +155,7 @@ export default function Track({manuscript=[]}) {
                     {/* Left side - Image Card */}
                     <div className="w-full md:w-1/2">
                     <Card isFooterBlurred radius="lg" className="border-none">
-                        <Image
-                        alt="Woman listing to music"
-                        className="object-cover w-full"
-                        height={300}
-                        src="https://nextui.org/images/hero-card.jpeg"
-                        width={250}
-                        />
+                        <iframe width={600} height={500} allowFullScreen src={selectedData.man_doc_content + '/preview'}/>
                         <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
                         <p className="text-tiny text-white/80">Available soon.</p>
                         <Button onClick={() => modifyDocument(selectedData.man_doc_content)} className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
